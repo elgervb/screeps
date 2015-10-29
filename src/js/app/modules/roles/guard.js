5a2035f54da995f0e426f92f8db142add3261a7b
elger <elgervb@gmail.com>
2015-10-29 22:15:58 +0100

    if nothing to do, then move to Flag1

--- src/js/app/modules/roles/guard.js
+++ src/js/app/modules/roles/guard.js
@@ -1,9 +1,13 @@
-/* global FIND_HOSTILE_CREEPS, ERR_NOT_IN_RANGE */
+/* global Game, FIND_HOSTILE_CREEPS, ERR_NOT_IN_RANGE */
 module.exports = (creep) => {
   let targets = creep.room.find(FIND_HOSTILE_CREEPS);
   if (targets.length) {
     if (creep.attack(targets[0]) === ERR_NOT_IN_RANGE) {
       creep.moveTo(targets[0]);
+    }
+  } else {
+    if (Game.flags.Flag1){
+      creep.moveTo(creep.room.find(Game.flags.Flag1));
     }
   }
 };
