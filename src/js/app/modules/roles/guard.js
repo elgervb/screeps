71e13eff615734feab7ae5c7f6bc99c3d692ea8f
elger <elgervb@gmail.com>
2015-10-30 00:03:14 +0100

    attack all in range of 15

--- src/js/app/modules/roles/guard.js
+++ src/js/app/modules/roles/guard.js
@@ -1,7 +1,7 @@
 /* global Game, FIND_HOSTILE_CREEPS, ERR_NOT_IN_RANGE */
 module.exports = (creep) => {
   // let targets = creep.room.find(FIND_HOSTILE_CREEPS);
-  let targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 3);
+  let targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 15);
   if (targets.length) {
     if (creep.attack(targets[0]) === ERR_NOT_IN_RANGE) {
       creep.moveTo(targets[0]);
