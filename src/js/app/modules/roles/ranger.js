2c50a037909e23572f0d23bdccadc1078e6c6956
Elger van Boxtel <elgervb@gmail.com>
2015-10-30 13:34:05 +0100

    do a ranged attack

--- src/js/app/modules/roles/ranger.js
+++ src/js/app/modules/roles/ranger.js
@@ -1,8 +1,14 @@
-/* global  */
-"use strict";
-
-let guard = require('guard');
-
-module.exports = function (creep) {
-  guard(creep);
+/* global Game, FIND_HOSTILE_CREEPS, ERR_NOT_IN_RANGE */
+module.exports = (creep) => {
+  // let targets = creep.room.find(FIND_HOSTILE_CREEPS);
+  let targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 15);
+  if (targets.length) {
+    if (creep.rangedAttack(targets[0]) === ERR_NOT_IN_RANGE) {
+      creep.moveTo(targets[0]);
+    }
+  } else {
+    if (Game.flags.Flag1){
+      creep.moveTo(Game.flags.Flag1);
+    }
+  }
 };
