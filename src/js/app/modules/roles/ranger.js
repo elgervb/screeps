5fb4d932265af777f54495d5f020368eb9e7090d
Elger van Boxtel <elgervb@gmail.com>
2015-10-30 09:46:14 +0100

    ranger used guard specifics

--- src/js/app/modules/roles/ranger.js
+++ src/js/app/modules/roles/ranger.js
@@ -1,14 +1,8 @@
-/* global Game, FIND_HOSTILE_CREEPS, ERR_NOT_IN_RANGE */
-module.exports = (creep) => {
-  // let targets = creep.room.find(FIND_HOSTILE_CREEPS);
-  let targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 15);
-  if (targets.length) {
-    if (creep.attack(targets[0]) === ERR_NOT_IN_RANGE) {
-      creep.moveTo(targets[0]);
-    }
-  } else {
-    if (Game.flags.Flag1){
-      creep.moveTo(Game.flags.Flag1);
-    }
-  }
+/* global  */
+"use strict";
+
+let guard = require('guard');
+
+module.exports = function (creep) {
+  guard(creep);
 };
