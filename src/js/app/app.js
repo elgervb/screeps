9eabc6b84416b52e8afdbbc71334081500db095d
Elger van Boxtel <elgervb@gmail.com>
2015-11-05 08:50:02 +0100

    require files when needed...

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -1,42 +1,34 @@
 /* global Game */
-let harvester = require('harvester');
-let guard = require('guard');
-let ranger = require('ranger');
-let builder = require('builder');
-let healer = require('healer');
-let test = require('test');
-let autospawn = require('autospawn');
-
 module.exports.loop = function () {
 
-  autospawn();
+  require('autospawn')();
 
   for (let name in Game.creeps) {
     if (name) {
       let creep = Game.creeps[name];
   
       if (creep.memory.role === 'harvester') {
-        harvester(creep);
+        require('harvester')(creep);
       }
   
       if (creep.memory.role === 'builder') {
-        builder(creep);
+        require('builder')(creep);
       }
   
       if (creep.memory.role === 'guard') {
-        guard(creep);
+        require('guard')(creep);
       }
       
       if (creep.memory.role === 'ranger') {
-        ranger(creep);
+        require('ranger')(creep);
       }
       
-       if (creep.memory.role === 'healer') {
-        healer(creep);
+      if (creep.memory.role === 'healer') {
+        require('healer')(creep);
       }
       
       if (creep.memory.role === 'test') {
-        test(creep);
+        require('test')(creep);
       }
     }
   }
