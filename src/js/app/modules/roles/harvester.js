e397f48a568c95841dae2e631368ce48e1332379
Elger van Boxtel <elgervb@gmail.com>
2015-11-05 12:12:10 +0100

    harvester optimizations

--- src/js/app/modules/roles/harvester.js
+++ src/js/app/modules/roles/harvester.js
@@ -1,12 +1,48 @@
-/* global Game, ERR_NOT_IN_RANGE, FIND_SOURCES */
+/* global Game, ERR_NOT_IN_RANGE, FIND_SOURCES_ACTIVE */
 module.exports = (creep) => {
-  if (creep.carry.energy < creep.carryCapacity) {
-    let sources = creep.room.find(FIND_SOURCES);
-    if (creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
-      creep.moveTo(sources[0]);
+  
+  let activeSource;
+  
+  let start = new Date().getTime();
+  let isFull = () => {
+    return creep.carry.energy === creep.carryCapacity;
+  };
+  let logPerf = (task) => {
+    let time = new Date().getTime() - start;
+    if (time > 5) {
+      console.log(`${time} - ${Game.time} - Harvester ${task}`);
     }
+  };
+  
+  // setup creep memory 
+  if (!creep.memory.activeSourceID) {
+    let source = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
+    if (source) {
+      creep.memory.activeSourceID = source.id;
+      activeSource = source;
+    }
+  } else {
+    activeSource = Game.getObjectById(creep.memory.activeSourceID);
   }
-  else if (creep.transferEnergy(Game.spawns.Spawn1) === ERR_NOT_IN_RANGE) {
-    creep.moveTo(Game.spawns.Spawn1);
+   
+  // check if energy should be transferred back home 
+  if (isFull() || (creep.carry.energy && creep.pos.isNearTo(Game.spawns.Spawn1))) {
+    // bring energy back home
+    if (creep.transferEnergy(Game.spawns.Spawn1) === ERR_NOT_IN_RANGE) {
+      creep.moveTo(Game.spawns.Spawn1);
+    }
+    logPerf('transferEnergy');
+    return;
   }
+  
+  // harvest resources
+  if (activeSource && !isFull()) {
+    if (creep.harvest(activeSource) === ERR_NOT_IN_RANGE) {
+      creep.moveTo(activeSource);
+    }
+    logPerf('harvest');
+    return;
+  }
+  
+  logPerf('idle');
 };
