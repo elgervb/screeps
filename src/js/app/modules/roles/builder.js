615016dae558da58ddba6d549f99fb08271093bf
Elger van Boxtel <elgervb@gmail.com>
2015-11-06 13:58:38 +0100

    upgrade controller

--- src/js/app/modules/roles/builder.js
+++ src/js/app/modules/roles/builder.js
@@ -1,8 +1,24 @@
-/* global Game, ERR_NOT_IN_RANGE, FIND_CONSTRUCTION_SITES, STRUCTURE_ROAD */
+/* global Game, ERR_NOT_IN_RANGE, FIND_CONSTRUCTION_SITES */
 module.exports = (creep) => {
   
+  let targets;
+  
+  /**
+   * Upgrade controller when it's level is below 1 or when there are no construction sites
+   * 
+   * @return {boolean} whether or not to upgrade the controller
+   */
+  let upgradeController = () => {
+    if (creep.room.controller && creep.room.controller.level < 2) {
+      return true;
+    }
+    
+    targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+    return !targets || targets.length === 0;
+  };
+  
   // only build when having more than ... energy
-  if (Game.spawns.Spawn1.energy <= 275 || !!Game.spawns.Spawn1.spawning) {
+  if (Game.spawns.Spawn1.energy <= 250 || !!Game.spawns.Spawn1.spawning) {
     if (creep.carry.energy === 0) { // let creep finish his energy first
       return;
     }
@@ -15,22 +31,17 @@ module.exports = (creep) => {
     }
     return;
   }
-   
-  // build construction
-  let targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-  if (targets.length) {
-    if (creep.build(targets[0]) === ERR_NOT_IN_RANGE) {
-      creep.moveTo(targets[0]);
-    }
-    return;
-  }
   
   // upgrade controller
-  if (creep.room.controller) {
+  if (upgradeController()) {
     if (creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE) {
-      creep.room.createConstructionSite(creep, STRUCTURE_ROAD); // build road to the controller
+      // creep.room.createConstructionSite(creep, STRUCTURE_ROAD); // build road to the controller
       creep.moveTo(creep.room.controller);
     }
-    return;
   }
+  // build construction sites
+  else if (creep.build(targets[0]) === ERR_NOT_IN_RANGE) {
+    creep.moveTo(targets[0]);
+  }
+  
 };
